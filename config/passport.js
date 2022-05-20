219a06be5c89c5bee795250e0ecb4c1aa4adde99
teste <joaopsoares3@outlook.com>
2022-05-19 22:10:09 -0300

    Atv12

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+var passport = require('passport');
+var GitHubStrategy = require('passport-github2').Strategy;
+var mongoose = require('mongoose');
+
+module.exports = function () {
+   var Usuario = mongoose.model('Usuario');
+
+   passport.use(
+      new GitHubStrategy(
+         {
+            clientID: 'b0ee32010650c88266d0',
+            clientSecret: '45bd882368da4d7af71e2d3238850ea62f8d29e1',
+            callbackURL: 'http://localhost:3000/auth/github/callback',
+         },
+         function (accessToken, refreshToken, profile, done) {
+            Usuario.findOrCreate(
+               { login: profile.username },
+               { nome: profile.username },
+               function (erro, usuario) {
+                  if (erro) {
+                     console.log(erro);
+                     return done(erro);
+                  }
+                  return done(null, usuario);
+               }
+            );
+         }
+      )
+   );
+
+   passport.serializeUser(function (usuario, done) {
+      done(null, usuario._id);
+   });
+
+   passport.deserializeUser(function (id, done) {
+      Usuario.findById(id)
+         .exec()
+         .then(function (usuario) {
+            done(null, usuario);
+         });
+   });
+};
